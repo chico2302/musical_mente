1446c6f3c2ad71059e7792bff815469d3059d26b
Francisco Ribeiro <chicobr1234@gmail.com>
2026-06-16 13:48:27 -0300

    Update firebaseConfig.js

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyD6OdJLCAabAvn6W67vOtedZBvhIh7JV-s",
   authDomain: "musicas-5acd8.firebaseapp.com",
@@ -15,7 +12,6 @@ const firebaseConfig = {
   appId: "1:136331303391:web:f95216f3d5cfaaf1160c6a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
